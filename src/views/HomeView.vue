5fedeb5d3b6ccabde47bdd38b370531d23d6e717
Ming Fai Cheung <mingfai@live.com>
2023-12-01 10:41:32 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import cars from "../data/cars.json"
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <div>
+        <h1>Home View</h1>
+        <div class="cars">
+            <RouterLink :to="`/cars/${car.id}`" v-for="car in cars"  :key="car.id">
+            {{ car.name }}
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.cars {
+  display:flex;
+}
+
+.cars a {
+    text-decoration: none;
+    color: black;
+    width: 100%;
+    box-shadow: 1px 1px 1px rgb(0,0,0,0.288);
+    margin: 10px;
+
+}
+
+</style>
